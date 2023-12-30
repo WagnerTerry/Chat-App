4c33f312b3f3e87fba07bd774ba5e05609d40118
Wagner Terry <goncalveswagner15@gmail.com>
2023-12-30 19:13:38 -0300

    feat: iniciando tela de home

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 - npm i sass
 - npm i react-router-dom
 - npm i axios
+- npm i socket.io-client
+
+## Configurando conexão
+Configure connection:
+- server url: https://dev.digitro.com
+- json config: {"path": "/callcontrol", "forceNew": true, "reconnectionAttempts": 3, "timeout": 2000}
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
+import React ,{useState, ChangeEvent} from 'react'
+// import {useNavigate} from 'react-router-dom'
 
-function App() {
+const App: React.FC = () => {
+  // const navigate = useNavigate();
+  const [name, setName] = useState('')
+  const [maxCalls, setMaxCalls] = useState('')
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  } 
+
+  const handleMaxCallsChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMaxCalls(e.target.value)
+  }
+
+  const handleJoinRoom = () => {
+    if(name.trim() !== '' && maxCalls.trim() !== ''){
+      console.log("entrar")
+      // navigate(`/chat?name=${encodeURIComponent(name)}&maxCalls=${encodeURIComponent(maxCalls)}`);
+
+    }
+  }
 
   return (
     <div>
+      <h1>Tela de conexão do agente</h1>
 
-      <h1>hello world</h1>
+      <div>
+        <label htmlFor="name">Usuário</label>
+        <input type="text" value={name} placeholder='Usuário' onChange={handleNameChange} />
+      </div>
+      <div>
+        <label htmlFor="maxCalls">Máximo de chamadas</label>
+        <input type="text" value={maxCalls} placeholder='Máximo de chamadas' onChange={handleMaxCallsChange} />
+      </div>
+      <button onClick={handleJoinRoom}>Conectar</button>
+
     </div>
-  )
-}
+  );
+};
 
 export default App
