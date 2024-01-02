e16c662b450d2b554c84db8a8af8b912cc23dc71
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-02 18:35:24 -0300

    chore: iniciando configuração com socket

--- src/views/AgentConnection/index.tsx
+++ src/views/AgentConnection/index.tsx
@@ -1,59 +1,86 @@
-import React, { useState, ChangeEvent } from 'react'
+import React, { useState, ChangeEvent, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { io, Socket } from 'socket.io-client';
 
-import "./style.scss"
+import './style.scss';
 
 const AgentConnection: React.FC = () => {
     const navigate = useNavigate();
-    const [username, setUsername] = useState('')
-    const [maxCalls, setMaxCalls] = useState('')
+    const [username, setUsername] = useState('');
+    const [maxCalls, setMaxCalls] = useState('');
+    const [socket, setSocket] = useState<Socket | null>(null);
+
+    useEffect(() => {
+        // Configuração das opções de conexão
+        const socketOptions = {
+            path: '/callcontrol',
+            forceNew: true,
+            reconnectionAttempts: 3,
+            timeout: 2000,
+        };
+
+        // Conectar ao servidor via WebSocket com as opções especificadas
+        const socket = io('https://dev.digitro.com/', socketOptions);
+        setSocket(socket);
+
+        // Verificar se a conexão foi bem-sucedida
+        socket.on('connect', () => {
+            console.log('Conectado ao servidor');
+        });
+
+        // Verificar se ocorreu algum erro durante a conexão
+        socket.on('connect_error', (error) => {
+            console.error('Erro de conexão:', error);
+        });
+
+    }, []);
 
     const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
         setUsername(e.target.value);
-    }
+    };
 
     const handleMaxCallsChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setMaxCalls(e.target.value)
-    }
+        setMaxCalls(e.target.value);
+    };
 
     const handleJoinRoom = () => {
-        if (username.trim() !== '' && maxCalls.trim() !== '') {
+        if (username.trim() !== '' && maxCalls.trim() !== '' && socket) {
+            // Enviar o evento USER_CONNECT para o servidor
+            socket.emit('USER_CONNECT', {
+                username,
+                maxCalls: parseInt(maxCalls, 10), // Converter para número
+            });
+
             navigate('/ServiceScreen', { state: { username, maxCalls } });
         }
-    }
+    };
 
     return (
         <div id="agent-connection">
             <h1>Tela de conexão do agente</h1>
 
-            <div className='connection-data'>
+            <div className="connection-data">
                 <div>
-                    <label
-                        htmlFor="name"
-                        className="input-label"
-                    >
+                    <label htmlFor="name" className="input-label">
                         Usuário
                     </label>
                     <input
                         type="text"
                         value={username}
                         className="styled-input"
-                        placeholder='Usuário'
+                        placeholder="Usuário"
                         onChange={handleNameChange}
                     />
                 </div>
                 <div>
-                    <label
-                        htmlFor="maxCalls"
-                        className="input-label"
-                    >
+                    <label htmlFor="maxCalls" className="input-label">
                         Máximo de chamadas
                     </label>
                     <input
                         type="text"
                         value={maxCalls}
                         className="styled-input"
-                        placeholder='Máximo de chamadas'
+                        placeholder="Máximo de chamadas"
                         onChange={handleMaxCallsChange}
                     />
                 </div>
@@ -65,9 +92,8 @@ const AgentConnection: React.FC = () => {
                     Conectar
                 </button>
             </div>
-
         </div>
     );
 };
 
-export default AgentConnection
+export default AgentConnection;
