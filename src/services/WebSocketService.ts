c5125563566482b180ec04fdb4dc25a0758cb5ba
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-03 20:19:41 -0300

    feat: reconectar usuario

--- src/services/WebSocketService.ts
+++ src/services/WebSocketService.ts
@@ -3,18 +3,16 @@ import { io, Socket } from 'socket.io-client';
 class WebSocketService {
     private static instance: WebSocketService;
     private socket: Socket | null = null;
+    private socketOptions = {
+        path: '/callcontrol',
+        forceNew: true,
+        reconnectionAttempts: 3,
+        timeout: 2000,
+    };
 
     private constructor() {
-        // Configuração das opções de conexão
-        const socketOptions = {
-            path: '/callcontrol',
-            forceNew: true,
-            reconnectionAttempts: 3,
-            timeout: 2000,
-        };
-
         // Conectar ao servidor via WebSocket com as opções especificadas
-        this.socket = io('https://dev.digitro.com/', socketOptions);
+        this.socket = io('https://dev.digitro.com/', this.socketOptions);
 
         // Verificar se a conexão foi bem-sucedida
         this.socket.on('connect', () => {
@@ -44,6 +42,21 @@ class WebSocketService {
             this.socket.disconnect();
             console.log('Desconectado do servidor');
         }
+    }
+
+    public reconnect() {
+        // Recriar a instância do socket
+        this.socket = io('https://dev.digitro.com/', this.socketOptions);
+
+        // Verificar se a conexão foi bem-sucedida
+        this.socket.on('connect', () => {
+            console.log('Reconectado ao servidor');
+        });
+
+        // Verificar se ocorreu algum erro durante a conexão
+        this.socket.on('connect_error', (error) => {
+            console.error('Erro de reconexão:', error);
+        });
     }
 }
 
