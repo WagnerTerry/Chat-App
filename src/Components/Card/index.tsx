4072cd8ccf24cff6ef713725e3c6996242267fe2
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-03 11:54:38 -0300

    chore: adicionando onClick no componente card

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -7,11 +7,12 @@ interface CardProps {
     title: string;
     subtitle: string;
     word: string;
+    onClick: () => void
 }
 
-const Card: React.FC<CardProps> = ({ icon, title, subtitle, word }) => {
+const Card: React.FC<CardProps> = ({ icon, title, subtitle, word, onClick }) => {
     return (
-        <div className="card">
+        <div className="card" onClick={onClick}>
             <div className="left-side">
                 <SvgIcon component={icon} className='icon' />
             </div>
